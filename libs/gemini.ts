c2ced0895b10abac59e567f2b83b8c7cb9575b6d
Anchal Jain <anchaljain1106@gmail.com>
2026-06-10 23:13:04 +0530

    downgrading gemini version

--- libs/gemini.ts
+++ libs/gemini.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 export const flashModel = genAI.getGenerativeModel({
-  model: "gemini-3.5-flash",
+  model: "gemini-2.5-flash",
   generationConfig: {
     responseMimeType: "application/json",
   },
